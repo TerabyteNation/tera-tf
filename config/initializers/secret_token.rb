36ded43824a5084b0041c7e255964b10b75352ac
redjazz96 <redjazz96@gmail.com>
2013-10-20 19:37:01 -0400

    Updates Rails to 4.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TeraGaming::Application.config.secret_token = '4f322b3047309d13cb8ae3db3fc9ab036e195c253cc8923e0318670ab240d79b5a4b51518c53c9bec4c702557fdaa40015b7e80b7df506b02c2816fbd49fdfe2'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeraTf::Application.config.secret_key_base = '76bd87d359095ca71ae3ea2b21e0560fcff749f3f860ecf9f34fb466b453e12c432f0f85ab96d1806c7babfdefd048f5214890211965ff0dee25c672a7346e3f'
